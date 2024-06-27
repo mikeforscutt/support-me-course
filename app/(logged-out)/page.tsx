8aa6528b10f21c5fb8c602349826a87f4450bad1
Mike Forscutt <mikeforscutt@gmail.com>
2024-06-27 09:21:29 +0100

    Added grouped folder for home page

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export default function LandingPage() {
+   return (
+      <div>
+         <h1>SupportMe</h1>
+         <Button>
+            Log in
+         </Button>
+         <Button>
+            Sign up
+         </Button>
+      </div>
+   )
+}
