7bbf12a619aef9625601e0de2990027d77a72b4f
mikeforscutt <mikeforscutt@gmail.com>
2024-12-20 13:41:05 +0000

    add calendar for sign up

--- app/(logged-out)/sign-up/page.tsx
+++ app/(logged-out)/sign-up/page.tsx
@@ -2,149 +2,213 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { PersonStandingIcon } from "lucide-react";
+import { CalendarIcon, PersonStandingIcon } from "lucide-react";
 import Link from "next/link";
-import * as z from 'zod';
+import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
 
-
-
-const formSchema = z.object({
-   email: z.string().email(),
-   accountType: z.enum(["personal", "company"]),
-   companyName: z.string().optional(),
-   numberOfEmployees: z.coerce.number().optional()
-
-}).superRefine((data, ctx) => {
-   if (data.accountType === "company" && !data.companyName) {
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    accountType: z.enum(["personal", "company"]),
+    companyName: z.string().optional(),
+    numberOfEmployees: z.coerce.number().optional(),
+    dob: z.date().refine((dob) => {
+      const today = new Date();
+      const eighteenYearsAgo = new Date(
+        today.getFullYear() - 18,
+        today.getMonth(),
+        today.getDate()
+      );
+      return dob <= eighteenYearsAgo;
+    }, "You must be 18 or older to sign up"),
+  })
+  .superRefine((data, ctx) => {
+    if (data.accountType === "company" && !data.companyName) {
       ctx.addIssue({
-         code: z.ZodIssueCode.custom,
-         path: ["companyName"],
-         message: "Company name is required"
-      })
-   }
-   if (data.accountType === "company" && (!data.numberOfEmployees || data.numberOfEmployees < 1)) {
+        code: z.ZodIssueCode.custom,
+        path: ["companyName"],
+        message: "Company name is required",
+      });
+    }
+    if (
+      data.accountType === "company" &&
+      (!data.numberOfEmployees || data.numberOfEmployees < 1)
+    ) {
       ctx.addIssue({
-         code: z.ZodIssueCode.custom,
-         path: ["numberOfEmployees"],
-         message: "Number of employees is required"
-      })
-   }
-})
+        code: z.ZodIssueCode.custom,
+        path: ["numberOfEmployees"],
+        message: "Number of employees is required",
+      });
+    }
+  });
 
 export default function SignupPage() {
-   const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-         email: "",
-      }
-   });
-   const handleSubmit = () => {
-      console.log("login validation passed");
-   }
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+  const handleSubmit = () => {
+    console.log("login validation passed");
+  };
 
-   const accountType = form.watch("accountType");
+  const accountType = form.watch("accountType");
 
-   return <>
+  return (
+    <>
       <PersonStandingIcon size={50} />
-      <Card className="w-full max-w-md">
-         <CardHeader>
-            <CardTitle>
-               Sign up
-            </CardTitle>
-            <CardDescription>
-               Sign up for a new SupportMe account
-            </CardDescription>
-         </CardHeader>
-         <CardContent>
-            <Form {...form}>
-               <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(handleSubmit)}>
+      <Card className='w-full max-w-md'>
+        <CardHeader>
+          <CardTitle>Sign up</CardTitle>
+          <CardDescription>Sign up for a new SupportMe account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              className='flex flex-col gap-4'
+              onSubmit={form.handleSubmit(handleSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name='email'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder='john@doe.com' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='accountType'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Account Type</FormLabel>
+                    <Select onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder='Select an account type' />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value='personal'>Personal</SelectItem>
+                        <SelectItem value='company'>Company</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              {accountType === "company" && (
+                <>
                   <FormField
-                     control={form.control}
-                     name="email"
-                     render={({ field }) => (
-                        <FormItem>
-                           <FormLabel>Email</FormLabel>
-                           <FormControl>
-                              <Input placeholder="john@doe.com" {...field} />
-                           </FormControl>
-                           <FormMessage />
-                        </FormItem>
-                     )}
+                    control={form.control}
+                    name='companyName'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Company name</FormLabel>
+                        <FormControl>
+                          <Input placeholder='Company name' {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
                   <FormField
-                     control={form.control}
-                     name="accountType"
-                     render={({ field }) => (
-                        <FormItem>
-                           <FormLabel>Account Type</FormLabel>
-                           <Select onValueChange={field.onChange}>
-                              <FormControl>
-                                 <SelectTrigger>
-                                    <SelectValue placeholder="Select an account type" />
-                                 </SelectTrigger>
-                              </FormControl>
-                              <SelectContent>
-                                 <SelectItem value="personal">
-                                    Personal
-                                 </SelectItem>
-                                 <SelectItem value="company">
-                                    Company
-                                 </SelectItem>
-                              </SelectContent>
-                           </Select>
-                        </FormItem>
-                     )}
+                    control={form.control}
+                    name='numberOfEmployees'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Employees</FormLabel>
+                        <FormControl>
+                          <Input
+                            type='number'
+                            min={0}
+                            placeholder='Employees'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                  {accountType === "company" &&
-                     <>
-                        <FormField
-                           control={form.control}
-                           name="companyName"
-                           render={({ field }) => (
-                              <FormItem>
-                                 <FormLabel>Company name</FormLabel>
-                                 <FormControl>
-                                    <Input placeholder="Company name" {...field} />
-                                 </FormControl>
-                                 <FormMessage />
-                              </FormItem>
-                           )}
+                </>
+              )}
+              <FormField
+                control={form.control}
+                name='dob'
+                render={({ field }) => (
+                  <FormItem className='flex flex-col pt-2'>
+                    <FormLabel>Date of birth</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant='outline'
+                            className='normal-case flex justify-between pr-1'
+                          >
+                            <span>Pick a date</span>
+                            <CalendarIcon />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent align='start' className='w-auto p-0'>
+                        <Calendar
+                          mode='single'
+                          defaultMonth={field.value}
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          disabled={(date) => date > new Date()}
+                          initialFocus
+                          fixedWeeks
+                          weekStartsOn={1}
+                          fromDate={new Date("1900-01-01")}
+                          toDate={new Date()}
                         />
-                        <FormField
-                           control={form.control}
-                           name="numberOfEmployees"
-                           render={({ field }) => (
-                              <FormItem>
-                                 <FormLabel>Employees</FormLabel>
-                                 <FormControl>
-                                    <Input type="number" min={0} placeholder="Employees" {...field} />
-                                 </FormControl>
-                                 <FormMessage />
-                              </FormItem>
-                           )}
-                        />
-                     </>
-                  }
-
-                  <Button type="submit">Sign up</Button>
-               </form>
-            </Form>
-         </CardContent>
-         <CardFooter className="justify-between">
-            <small>Already have an account?</small>
-            <Button asChild variant="outline" size="sm">
-               <Link href="/login">
-                  Login
-               </Link>
-            </Button>
-         </CardFooter>
-
+                      </PopoverContent>
+                    </Popover>
+                  </FormItem>
+                )}
+              />
+              <Button type='submit'>Sign up</Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className='justify-between'>
+          <small>Already have an account?</small>
+          <Button asChild variant='outline' size='sm'>
+            <Link href='/login'>Login</Link>
+          </Button>
+        </CardFooter>
       </Card>
-   </>;
-
+    </>
+  );
 }
