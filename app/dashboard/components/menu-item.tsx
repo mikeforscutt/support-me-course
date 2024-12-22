037f6f65c703ac91dc406598f1847f0ce502ccde
mikeforscutt <mikeforscutt@gmail.com>
2024-12-22 17:00:54 +0000

    add routing for additional pages

--- app/dashboard/components/menu-item.tsx
+++ app/dashboard/components/menu-item.tsx
@@ -13,15 +13,17 @@ export default function MenuItem({ children, href }: Props) {
   const pathName = usePathname();
   const isActive = pathName === href;
   return (
-    <Link
-      className={cn(
-        "block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground",
-        isActive &&
-          "bg-pink-500 hover:bg-pink-500 dark:hover:bg-pink-500 hover-text-foreground text-foreground"
-      )}
-      href={href}
-    >
-      {children}
-    </Link>
+    <li>
+      <Link
+        className={cn(
+          "block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground",
+          isActive &&
+            "bg-pink-500 hover:bg-pink-500 dark:hover:bg-pink-500 hover-text-primary-foreground text-primary-foreground"
+        )}
+        href={href}
+      >
+        {children}
+      </Link>
+    </li>
   );
 }
